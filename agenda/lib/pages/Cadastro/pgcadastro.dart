6585176a1771595c2019d293e66b21aa8031352f
Corvonauta <corvonauta@outlook.com>
2021-03-28 20:33:47 -0300

    login e cadastro com tratamento de entradas

--- agenda/lib/pages/Cadastro/pgcadastro.dart
+++ agenda/lib/pages/Cadastro/pgcadastro.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class JanelaCadastro extends StatefulWidget {
   const JanelaCadastro({Key key}) : super(key: key);
-
   @override
   _JanelaCadastroState createState() => _JanelaCadastroState();
 }
+
+int corNome = Colors.white.value;
+int corEmail = Colors.white.value;
+int corLogin = Colors.white.value;
+int corSenha = Colors.white.value;
+int corAviso = Colors.transparent.value;
+String txtAviso = "Deu ruim";
 
 class _JanelaCadastroState extends State<JanelaCadastro> {
   @override
@@ -13,6 +21,113 @@ class _JanelaCadastroState extends State<JanelaCadastro> {
     double totalWidth = MediaQuery.of(context).size.width;
     double totalHeight = MediaQuery.of(context).size.height;
     double geralScale = (totalWidth < totalHeight) ? totalWidth : totalHeight;
+
+    RegExp expEmail = new RegExp("[a-zA-Z_0-9]+@[a-z]+([\.]{1}[a-z]+)+");
+
+    String funcCadastro(String nome, String email, String login, String senha) {
+      //"MACHINES" - Morpheus, 2199
+      return "Retornar problemas";
+    }
+
+    Text txtErro = new Text(txtAviso,
+        textScaleFactor: (geralScale * 0.003),
+        style: new TextStyle(color: Color(corAviso)));
+
+    TextField fieldNome = new TextField(
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.allow(RegExp("[a-zA-Z ]+"))
+        ],
+        controller: new TextEditingController(),
+        decoration: new InputDecoration(
+            labelText: "Nome",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0.0))),
+            filled: true,
+            fillColor: Color(corNome)));
+
+    TextField fieldEmail = new TextField(
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.deny(RegExp("[\",/\\\\]"))
+        ],
+        controller: new TextEditingController(),
+        decoration: new InputDecoration(
+            labelText: "Email",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0.0))),
+            filled: true,
+            fillColor: Color(corEmail)));
+
+    TextField fieldLogin = new TextField(
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.deny(RegExp("[\",/\\\\]"))
+        ],
+        controller: new TextEditingController(),
+        decoration: new InputDecoration(
+            labelText: "Usuário",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0.0))),
+            filled: true,
+            fillColor: Color(corLogin)));
+
+    TextField fieldSenha = new TextField(
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.deny(RegExp("[\",/\\\\]"))
+        ],
+        controller: new TextEditingController(),
+        obscureText: true,
+        obscuringCharacter: "•",
+        decoration: new InputDecoration(
+            labelText: "Senha",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0.0))),
+            filled: true,
+            fillColor: Color(corSenha)));
+
+    TextButton btnCadastrar = new TextButton(
+        onPressed: () {
+          corAviso = Colors.transparent.value;
+          (fieldNome.controller.text == "")
+              ? corNome = 0xFFE57373
+              : corNome = 0xFFFFFFFF;
+          (fieldEmail.controller.text == "")
+              ? corEmail = 0xFFE57373
+              : corEmail = 0xFFFFFFFF;
+          (fieldLogin.controller.text == "")
+              ? corLogin = 0xFFE57373
+              : corLogin = 0xFFFFFFFF;
+          (fieldSenha.controller.text == "")
+              ? corSenha = 0xFFE57373
+              : corSenha = 0xFFFFFFFF;
+
+          if (expEmail.hasMatch(fieldEmail.controller.text) &&
+              (fieldEmail.controller.text ==
+                  expEmail.stringMatch(fieldEmail.controller.text))) {
+            if (fieldNome.controller.text != "" &&
+                fieldLogin.controller.text != "" &&
+                fieldSenha.controller.text != "") {
+              txtAviso = funcCadastro(
+                  fieldNome.controller.text,
+                  fieldEmail.controller.text,
+                  fieldLogin.controller.text,
+                  fieldSenha.controller.text);
+              if (txtAviso == "")
+                print("Cadastrado");
+              else
+                corAviso = Colors.red.value;
+            }
+          } else
+            corEmail = 0xFFE57373;
+
+          setState(() {});
+        },
+        child: Container(
+            alignment: AlignmentDirectional.center,
+            height: (geralScale * 0.1),
+            width: (geralScale * 0.35),
+            decoration: BoxDecoration(color: Colors.black),
+            child: Text("Enviar",
+                textScaleFactor: geralScale * 0.005,
+                style: TextStyle(color: Colors.white))));
 
     return Scaffold(
         body: Center(
@@ -33,63 +148,24 @@ class _JanelaCadastroState extends State<JanelaCadastro> {
                 Container(
                     height: (geralScale * 0.09),
                     width: (geralScale * 0.6),
-                    child: TextField(
-                        decoration: (new InputDecoration(
-                            labelText: "Nome",
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0.0))),
-                            filled: true,
-                            fillColor: Colors.white)))),
+                    child: fieldNome),
                 Container(height: geralScale * 0.02),
                 Container(
                     height: (geralScale * 0.09),
                     width: (geralScale * 0.6),
-                    child: TextField(
-                        decoration: (new InputDecoration(
-                            labelText: "Email",
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0.0))),
-                            filled: true,
-                            fillColor: Colors.white)))),
+                    child: fieldEmail),
                 Container(height: geralScale * 0.02),
                 Container(
                     height: (geralScale * 0.09),
                     width: (geralScale * 0.6),
-                    child: TextField(
-                        decoration: (new InputDecoration(
-                            labelText: "Usuário",
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0.0))),
-                            filled: true,
-                            fillColor: Colors.white)))),
+                    child: fieldLogin),
                 Container(height: geralScale * 0.02),
                 Container(
                     height: (geralScale * 0.09),
                     width: (geralScale * 0.6),
-                    child: TextField(
-                        obscureText: true,
-                        obscuringCharacter: "•",
-                        decoration: (new InputDecoration(
-                            labelText: "Senha",
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0.0))),
-                            filled: true,
-                            fillColor: Colors.white)))),
-                Container(height: geralScale * 0.05),
-                TextButton(
-                    onPressed: null,
-                    child: Container(
-                        alignment: AlignmentDirectional.center,
-                        height: (geralScale * 0.1),
-                        width: (geralScale * 0.35),
-                        decoration: BoxDecoration(color: Colors.black),
-                        child: Text("Enviar",
-                            textScaleFactor: geralScale * 0.005,
-                            style: TextStyle(color: Colors.white)))),
+                    child: fieldSenha),
+                Container(height: (geralScale * 0.05), child: txtErro),
+                btnCadastrar,
                 Container(height: geralScale * 0.035)
               ]))
     ]))));
--- agenda/lib/pages/Login/pglogin.dart
+++ agenda/lib/pages/Login/pglogin.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:agenda/pages/Cadastro/pgcadastro.dart';
 import 'package:agenda/pages/Tela_inicial/pgtelaini.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class JanelaLogin extends StatefulWidget {
   const JanelaLogin({Key key}) : super(key: key);
@@ -10,6 +13,10 @@ class JanelaLogin extends StatefulWidget {
   _JanelaLoginState createState() => _JanelaLoginState();
 }
 
+int corLogin = Colors.white.value;
+int corSenha = Colors.white.value;
+int corAviso = Colors.transparent.value;
+
 class _JanelaLoginState extends State<JanelaLogin> {
   @override
   Widget build(BuildContext context) {
@@ -17,16 +24,91 @@ class _JanelaLoginState extends State<JanelaLogin> {
     double totalHeight = MediaQuery.of(context).size.height;
     double geralScale = (totalWidth < totalHeight) ? totalWidth : totalHeight;
 
+    bool funcLogin(String login, String senha) {
+      //"MACHINES" - Morpheus, 2199
+      return true;
+    }
+
+    Text txtErro = new Text("Login ou senha incorretos",
+        textScaleFactor: (geralScale * 0.003),
+        style: new TextStyle(color: Color(corAviso)));
+
+    BoxDecoration quadradoCentral = new BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black, style: BorderStyle.solid));
+
+    TextField fieldLogin = new TextField(
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.deny(RegExp("[\",/\\\\]"))
+        ],
+        controller: new TextEditingController(),
+        decoration: new InputDecoration(
+            labelText: "Usuário",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0.0))),
+            filled: true,
+            fillColor: Color(corLogin)));
+
+    TextField fieldSenha = new TextField(
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.deny(RegExp("[\",/\\\\]"))
+        ],
+        controller: new TextEditingController(),
+        obscureText: true,
+        obscuringCharacter: "•",
+        decoration: new InputDecoration(
+            labelText: "Senha",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0.0))),
+            filled: true,
+            fillColor: Color(corSenha)));
+
+    TextButton btnLogin = new TextButton(
+        onPressed: () {
+          corAviso = Colors.transparent.value;
+          if (fieldLogin.controller.text == "")
+            corLogin = 0xFFE57373;
+          else
+            corLogin = 0xFFFFFFFF;
+
+          if (fieldSenha.controller.text == "")
+            corSenha = 0xFFE57373;
+          else
+            corSenha = 0xFFFFFFFF;
+
+          if (fieldLogin.controller.text != "" &&
+              fieldSenha.controller.text !=
+                  "") if (funcLogin(
+              fieldLogin.controller.text, fieldSenha.controller.text))
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => PgTelaIni()));
+          else
+            corAviso = Colors.red.value;
+          setState(() {});
+        },
+        child: Container(
+            alignment: AlignmentDirectional.center,
+            height: (geralScale * 0.1),
+            width: (geralScale * 0.4),
+            decoration: BoxDecoration(color: Colors.black),
+            child: Text("Entrar",
+                textScaleFactor: geralScale * 0.006,
+                style: TextStyle(color: Colors.white))));
+
+    TextButton btnCadastrar = new TextButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => JanelaCadastro()));
+        },
+        child: Text("Cadastrar", textScaleFactor: (geralScale * 0.003)));
+
     return Scaffold(
         body: Center(
             child: SingleChildScrollView(
                 child: Wrap(children: <Widget>[
       Container(
           width: (geralScale * 0.7),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              border:
-                  Border.all(color: Colors.black, style: BorderStyle.solid)),
+          decoration: quadradoCentral,
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -36,51 +118,15 @@ class _JanelaLoginState extends State<JanelaLogin> {
                 Container(
                     height: (geralScale * 0.1),
                     width: (geralScale * 0.6),
-                    child: TextField(
-                        decoration: (new InputDecoration(
-                            labelText: "Usuário",
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0.0))),
-                            filled: true,
-                            fillColor: Colors.white)))),
+                    child: fieldLogin),
                 Container(height: geralScale * 0.02),
                 Container(
                     height: (geralScale * 0.1),
                     width: (geralScale * 0.6),
-                    child: TextField(
-                        obscureText: true,
-                        obscuringCharacter: "•",
-                        decoration: (new InputDecoration(
-                            labelText: "Senha",
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0.0))),
-                            filled: true,
-                            fillColor: Colors.white)))),
-                Container(height: geralScale * 0.06),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => PgTelaIni()));
-                    },
-                    child: Container(
-                        alignment: AlignmentDirectional.center,
-                        height: (geralScale * 0.1),
-                        width: (geralScale * 0.4),
-                        decoration: BoxDecoration(color: Colors.black),
-                        child: Text("Entrar",
-                            textScaleFactor: geralScale * 0.006,
-                            style: TextStyle(color: Colors.white)))),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => JanelaCadastro()));
-                    },
-                    child: Text("Cadastrar",
-                        textScaleFactor: (geralScale * 0.003))),
+                    child: fieldSenha),
+                Container(height: (geralScale * 0.06), child: txtErro),
+                btnLogin,
+                btnCadastrar,
                 Container(height: geralScale * 0.03)
               ]))
     ]))));
