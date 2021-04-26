326ddac98cf855f4d98b873e731b22c7e0bd878f
Corvonauta-dev <78376569+Corvonauta-dev@users.noreply.github.com>
2021-04-26 11:40:08 -0300

    Delete agenda/lib/application/bindigs directory

--- agenda/lib/application/bindigs/application_bindings.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:agenda/repositories/materiaRepository.dart';
-// import 'package:get/get.dart';
-
-// class ApplicationBindings implements Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut(() => MateriaRepository());
-//   }
-// }
